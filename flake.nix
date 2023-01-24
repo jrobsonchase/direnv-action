231b1598208e9daf686df29c1e83e6a663939428
Josh Robson Chase <josh@robsonchase.com>
2023-01-24 11:17:36 -0500

    Add ci

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "direnv composite github action";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      });
+}
